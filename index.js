4c6b8b1ce6526813a2de11fa14942f9dbbafa701
emilhajizada <emil.hajizada@code.edu.az>
2023-06-13 23:21:20 +0400

    added

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use("/static", express.static("public"));
+
+app.use(cors());
+
+let id_count = 8;
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("adss");
+});
+
+app.get("/:id", (req, res) => {
+  res.send(data.find((x) => x.id === +req.params.id));
+});
+
+app.post("/", (req, res) => {
+  res.send("men post sorgusuyam");
+});
+
+app.put("/:id", (req, res) => {
+  res.send("men put sorgusuyam");
+});
+
+app.delete("/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
